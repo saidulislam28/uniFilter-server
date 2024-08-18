9f033d63cb274dcc69f74d9d1c65a0007744b46b
saidulislam28 <saidulislam79028@gmail.com>
2024-08-18 17:53:43 +0600

    deployed done

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('uniFilter').collection('products');
 
