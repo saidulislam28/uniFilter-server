19bc499f48fbded056c6015af5b426ee8d3acb8b
saidulislam28 <saidulislam79028@gmail.com>
2024-08-18 17:37:48 +0600

    successfully authentication done

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run() {
     
     
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. MongoDB!");
   } finally {
     
   }
