43e33428c392833ca7ce2df01509ff3435b4330e
saidulislam28 <saidulislam79028@gmail.com>
2024-08-18 18:11:40 +0600

    add some text

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+//middleware 
+
 app.use(cors());
 app.use(express.json());
 
+
+// connecting mongo with credentials
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9imp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -20,9 +24,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
-
+   
+// product collection
     const productCollection = client.db('uniFilter').collection('products');
+
+// for getting products from database
 
     app.get('/products', async (req, res) => {
       const { page = 1, limit = 10, search = "", category = "", priceRange = "", sort = "" } = req.query;
